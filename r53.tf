56ce4f92de6d8dc235b856125c879fb3b1b6dc64
Xander Harris <plebis@hotmail.com>
2021-07-30 23:09:30 -0700

    Drop hamdance net
    See #7

--- r53.tf
+++ r53.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_zone" "hamdance" {
-  name = "hamdance.net"
-}
+# resource "aws_route53_zone" "hamdance" {
+#   name = "hamdance.net"
+# }
 
 resource "aws_route53_record" "hamdance" {
   zone_id = aws_route53_zone.hamdance.zone_id
