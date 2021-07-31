44f22ed0810cf2c7312d17b904e0b2a5d33f24f1
Xander Harris <plebis@hotmail.com>
2021-07-30 22:45:19 -0700

    Add dependency
    See #6

--- r53.tf
+++ r53.tf
@@ -6,6 +6,9 @@ resource "aws_route53_record" "hamdance" {
   zone_id = aws_route53_zone.hamdance.zone_id
   name    = "hamdance.net"
   type    = "A"
+  depends_on = [
+    aws_route53_zone.hamdance
+  ]
 
   alias {
     name                   = aws_lb.rearc.dns_name
