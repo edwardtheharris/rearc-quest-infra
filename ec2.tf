05ef2386ccf539bcd80beacbdd9b0ca390e2ef4d
Xander Harris <plebis@hotmail.com>
2021-07-30 18:57:29 -0700

    Fix instance deploy and security group
    See #2

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "allow-ec2" {
     description      = "All from everywhere"
     from_port        = 0
     to_port          = 0
-    protocol         = "tcp"
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
@@ -45,7 +45,7 @@ resource "aws_instance" "rearc" {
   ami            = data.aws_ami.amazon.id
   instance_type  = "t3.micro"
   key_name       = aws_key_pair.rearc.key_name
-  security_groups = [aws_security_group.allow-ec2.id]
+  vpc_security_group_ids = [aws_security_group.allow-ec2.id]
   user_data      = file("ec2/user-data.sh")
 
   depends_on = [
