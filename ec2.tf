0dd11c83d879b52fe192131c28e466ee3c257e06
Xander Harris <plebis@hotmail.com>
2021-07-30 18:35:45 -0700

    Set user data script
    See #2

--- ec2.tf
+++ ec2.tf
@@ -8,9 +8,16 @@ data "aws_ami" "amazon" {
   }
 }
 
+resource "aws_key_pair" "rearc" {
+  key_name   = "rearc"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "rearc" {
   ami           = data.aws_ami.amazon.id
   instance_type = "t3.micro"
+  key_name = aws_key_pair.rearc.key_name
+  user_data = file("ec2/user-data.sh")
 
   tags = {
     Name = "Rearc Quest"
