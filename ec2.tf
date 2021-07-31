944c39597009220f8ec68489bda3da49e71bec04
Xander Harris <plebis@hotmail.com>
2021-07-30 23:30:21 -0700

    Add tls hamdance.net
    See #7

--- ec2.tf
+++ ec2.tf
@@ -153,10 +153,28 @@ resource "aws_lb_target_group" "rearc" {
   vpc_id   = data.aws_vpc.default.id
 }
 
-resource "aws_lb_listener" "rearc" {
+resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.rearc.arn
   port              = "80"
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "rearc-ssl" {
+  load_balancer_arn = aws_lb.rearc.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.hamdance.arn
 
   default_action {
     type             = "forward"
