79302cca5ac4a8417a883fdff44aa0b3be030f23
Xander Harris <plebis@hotmail.com>
2021-07-30 23:35:42 -0700

    Add tags
    See #7

--- ec2.tf
+++ ec2.tf
@@ -153,21 +153,25 @@ resource "aws_lb_target_group" "rearc" {
   vpc_id   = data.aws_vpc.default.id
 }
 
-# resource "aws_lb_listener" "rearc-redirect" {
-#   load_balancer_arn = aws_lb.rearc.arn
-#   port              = "80"
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "rearc-redirect" {
+  load_balancer_arn = aws_lb.rearc.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-#   default_action {
-#     type = "redirect"
+  default_action {
+    type = "redirect"
 
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  tags {
+    Purpose = "redirect"
+  }
+}
 
 resource "aws_lb_listener" "rearc-ssl" {
   load_balancer_arn = aws_lb.rearc.arn
@@ -179,6 +183,10 @@ resource "aws_lb_listener" "rearc-ssl" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.rearc.arn
+  }
+
+  tags {
+    purpose = "forward"
   }
 }
 
