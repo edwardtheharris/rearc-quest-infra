d1aea6bd04382207661873c87f69f7459da890ed
Xander Harris <plebis@hotmail.com>
2021-07-30 22:44:02 -0700

    hamdance
    See #6

--- r53.tf
+++ r53.tf
@@ -1,3 +1,15 @@
-resource "aws_route53_zone" "primary" {
+resource "aws_route53_zone" "hamdance" {
   name = "hamdance.net"
+}
+
+resource "aws_route53_record" "hamdance" {
+  zone_id = aws_route53_zone.hamdance.zone_id
+  name    = "hamdance.net"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.rearc.dns_name
+    zone_id                = aws_lb.rearc.zone_id
+    evaluate_target_health = true
+  }
 }
