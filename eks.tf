ccaf4581bc6f79df552718b41f868de722559bad
Xander Harris <plebis@hotmail.com>
2021-07-31 12:57:56 -0700

    Drop restriction
    See #27

--- eks.tf
+++ eks.tf
@@ -4,9 +4,6 @@ resource "aws_eks_cluster" "default" {
 
   version = "1.21"
   vpc_config {
-    public_access_cidrs = [
-      var.eks_access_cidr
-    ]
     security_group_ids = [
       aws_security_group.allow-ec2.id
     ]
