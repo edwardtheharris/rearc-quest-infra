ced5af87c4fe5e05e7893250040dd81c80fb847c
Xander Harris <plebis@hotmail.com>
2021-07-31 09:22:36 -0700

    Add node groups
    See #27

--- eks.tf
+++ eks.tf
@@ -18,16 +18,74 @@ resource "aws_eks_cluster" "default" {
   ]
 }
 
-resource "aws_eks_node_group" "default" {
+resource "aws_eks_node_group" "us-west-2a" {
   cluster_name    = aws_eks_cluster.default.name
-  node_group_name = "default"
+  node_group_name = "us-west-2a"
   node_role_arn   = aws_iam_role.eksNodeGroup.arn
-  subnet_ids = [
-    data.aws_subnet.us-west-2a.id,
-    data.aws_subnet.us-west-2b.id,
-    data.aws_subnet.us-west-2c.id,
-    data.aws_subnet.us-west-2d.id
+  subnet_ids = [data.aws_subnet.us-west-2a.id]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
+}
+
+resource "aws_eks_node_group" "us-west-2b" {
+  cluster_name    = aws_eks_cluster.default.name
+  node_group_name = "us-west-2b"
+  node_role_arn   = aws_iam_role.eksNodeGroup.arn
+  subnet_ids = [data.aws_subnet.us-west-2b.id]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_eks_node_group" "us-west-2c" {
+  cluster_name    = aws_eks_cluster.default.name
+  node_group_name = "us-west-2c"
+  node_role_arn   = aws_iam_role.eksNodeGroup.arn
+  subnet_ids = [data.aws_subnet.us-west-2c.id]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_eks_node_group" "us-west-2d" {
+  cluster_name    = aws_eks_cluster.default.name
+  node_group_name = "us-west-2d"
+  node_role_arn   = aws_iam_role.eksNodeGroup.arn
+  subnet_ids = [data.aws_subnet.us-west-2d.id]
 
   scaling_config {
     desired_size = 1
