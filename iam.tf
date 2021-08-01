86097df60a949edc9d2415334fa5513f805afb35
Xander Harris <plebis@hotmail.com>
2021-07-31 20:47:00 -0700

    Add oidc provider
    See #41

--- iam.tf
+++ iam.tf
@@ -153,35 +153,35 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   role       = aws_iam_role.eksNodeRole.name
 }
 
-# data "tls_certificate" "eks" {
-#   url = aws_eks_cluster.default.identity[0].oidc[0].issuer
-# }
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.default.identity[0].oidc[0].issuer
+}
 
-# resource "aws_iam_openid_connect_provider" "eks" {
-#   client_id_list  = ["sts.amazonaws.com"]
-#   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-#   url             = aws_eks_cluster.default.identity[0].oidc[0].issuer
-# }
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.default.identity[0].oidc[0].issuer
+}
 
-# data "aws_iam_policy_document" "eks-oidc" {
-#   statement {
-#     actions = ["sts:AssumeRoleWithWebIdentity"]
-#     effect  = "Allow"
+data "aws_iam_policy_document" "eks-oidc" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
 
-#     condition {
-#       test     = "StringEquals"
-#       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-#       values   = ["system:serviceaccount:kube-system:aws-node"]
-#     }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
 
-#     principals {
-#       identifiers = [aws_iam_openid_connect_provider.eks.arn]
-#       type        = "Federated"
-#     }
-#   }
-# }
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
 
-# resource "aws_iam_role" "example" {
-#   assume_role_policy = data.aws_iam_policy_document.eks-oidc.json
-#   name               = "example"
-# }
+resource "aws_iam_role" "eks-alb-controller" {
+  assume_role_policy = data.aws_iam_policy_document.eks-oidc.json
+  name               = "eks-alb-controller"
+}
