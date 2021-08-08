cd31fbcc255a0db1b5fa0261dc7cf9afe2b470e1
Xander Harris <plebis@hotmail.com>
2021-08-08 10:42:20 -0700

    Add back subnets
    See #54

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
-# data "aws_vpc" "default" {
-#   id = "vpc-af3030d7"
-# }
+data "aws_vpc" "default" {
+  id = "vpc-af3030d7"
+}
 
 # data "aws_route_table" "default" {
 #   route_table_id = "rtb-4de50837"
@@ -22,49 +22,49 @@
 #   id = "subnet-d316f2f9"
 # }
 
-# resource "aws_subnet" "us-west-2a" {
-#   cidr_block = "172.31.16.0/20"
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "us-west-2a"
-#     "kubernetes/cluster/default" = "shared"
-#     "kubernetes.io/role/elb" = "1"
-#   }
-#   vpc_id = data.aws_vpc.default.id
-# }
+resource "aws_subnet" "us-west-2a" {
+  cidr_block = "172.31.16.0/20"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "us-west-2a"
+    "kubernetes/cluster/default" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
 
-# resource "aws_subnet" "us-west-2b" {
-#   cidr_block = "172.31.32.0/20"
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "us-west-2b"
-#     "kubernetes/cluster/default" = "shared"
-#     "kubernetes.io/role/elb" = "1"
-#   }
-#   vpc_id = data.aws_vpc.default.id
-# }
+resource "aws_subnet" "us-west-2b" {
+  cidr_block = "172.31.32.0/20"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "us-west-2b"
+    "kubernetes/cluster/default" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
 
-# resource "aws_subnet" "us-west-2c" {
-#   cidr_block = "172.31.0.0/20"
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "us-west-2c"
-#     "kubernetes/cluster/default" = "shared"
-#     "kubernetes.io/role/elb" = "1"
-#   }
-#   vpc_id = data.aws_vpc.default.id
-# }
+resource "aws_subnet" "us-west-2c" {
+  cidr_block = "172.31.0.0/20"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "us-west-2c"
+    "kubernetes/cluster/default" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
 
-# resource "aws_subnet" "us-west-2d" {
-#   cidr_block = "172.31.48.0/20"
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "us-west-2d"
-#     "kubernetes/cluster/default" = "shared"
-#     "kubernetes.io/role/elb" = "1"
-#   }
-#   vpc_id = data.aws_vpc.default.id
-# }
+resource "aws_subnet" "us-west-2d" {
+  cidr_block = "172.31.48.0/20"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "us-west-2d"
+    "kubernetes/cluster/default" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
 
 # resource "aws_route_table_association" "us-west-2a" {
 #   subnet_id      = data.aws_subnet.us-west-2a.id
