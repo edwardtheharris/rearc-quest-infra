760f5a70d220ebc8d4c2128d55c12a2a0a2f5f12
Xander Harris <plebis@hotmail.com>
2021-08-08 10:30:50 -0700

    comment outputs
    See #54

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
-output "ecr_repository" {
-  value = aws_ecr_repository.rearc-quest.repository_url
-}
-
-# output "endpoint" {
-#   value = aws_eks_cluster.default.endpoint
+# output "ecr_repository" {
+#   value = aws_ecr_repository.rearc-quest.repository_url
 # }
 
-# output "kubeconfig-certificate-authority-data" {
-#   value = aws_eks_cluster.default.certificate_authority[0].data
+# # output "endpoint" {
+# #   value = aws_eks_cluster.default.endpoint
+# # }
+
+# # output "kubeconfig-certificate-authority-data" {
+# #   value = aws_eks_cluster.default.certificate_authority[0].data
+# # }
+
+# output "password" {
+#   value = aws_iam_user_login_profile.admin.encrypted_password
 # }
 
-output "password" {
-  value = aws_iam_user_login_profile.admin.encrypted_password
-}
+# output "access_key_id" {
+#   value = aws_iam_access_key.admin.id
+# }
 
-output "access_key_id" {
-  value = aws_iam_access_key.admin.id
-}
-
-output "secret_access_key" {
-  value = aws_iam_access_key.admin.encrypted_secret
-}
+# output "secret_access_key" {
+#   value = aws_iam_access_key.admin.encrypted_secret
+# }
