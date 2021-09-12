f86cbab5c2bb1244e603f7b78364c9f18abaf9e3
Xander Harris <xandertheharris@gmail.com>
2021-09-12 16:03:30 -0700

    Uncomment vars map
    See #57

--- test/terraform_aws_network_test.go
+++ test/terraform_aws_network_test.go
@@ -17,11 +17,11 @@ func TestTerraformAwsNetworkExample(t *testing.T) {
 	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
 
 	// Give the VPC and the subnets correct CIDRs
-	// vpcCidr := "172.32.0.0/16"
-	// subnetACidr := "172.32.16.0/20"
-	// subnetBCidr := "172.32.32.0/20"
-	// subnetCCidr := "172.32.48.0/20"
-	// subnetDCidr := "172.32.64.0/20"
+	vpcCidr := "172.32.0.0/16"
+	subnetACidr := "172.32.16.0/20"
+	subnetBCidr := "172.32.32.0/20"
+	subnetCCidr := "172.32.48.0/20"
+	subnetDCidr := "172.32.64.0/20"
 
 	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
 	// terraform testing.
@@ -30,14 +30,14 @@ func TestTerraformAwsNetworkExample(t *testing.T) {
 		TerraformDir: "../",
 
 		// Variables to pass to our Terraform code using -var options
-		// Vars: map[string]interface{}{
-		// 	"vpc_cidr":      vpcCidr,
-		// 	"subnet_a_cidr": subnetACidr,
-		// 	"subnet_b_cidr": subnetBCidr,
-		// 	"subnet_c_cidr": subnetCCidr,
-		// 	"subnet_d_cidr": subnetDCidr,
-		// 	"aws_region":    awsRegion,
-		// },
+		Vars: map[string]interface{}{
+			"vpc_cidr":      vpcCidr,
+			"subnet_a_cidr": subnetACidr,
+			"subnet_b_cidr": subnetBCidr,
+			"subnet_c_cidr": subnetCCidr,
+			"subnet_d_cidr": subnetDCidr,
+			"aws_region":    awsRegion,
+		},
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
